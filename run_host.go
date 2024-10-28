2680c77ca9130a81c89d32b228b71f8b216d299c
Matt Strapp <matt@mattstrapp.net>
2024-10-28 13:25:17 -0500

    Add icon
    Signed-off-by: Matt Strapp <matt@mattstrapp.net>

--- run_host.go
+++ run_host.go
@@ -10,5 +10,10 @@ import (
 func command(distribution Distribution, config Config) (*exec.Cmd, error) {
 	// Append the additional arguments to the command
 	args := append([]string{"-d", distribution.Name}, config.WslArgs...)
+	// Set the title of the terminal window
+	err := exec.Command("cmd", "/c", "title", distribution.Name).Run()
+	if err != nil {
+		return nil, err
+	}
 	return exec.Command("wsl", args...), nil
 }
